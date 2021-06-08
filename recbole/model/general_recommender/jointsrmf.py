66fdd53b654d0a4f915480cde5f411c5bf8cb8fd
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2021-06-08 11:44:15 +0200

    not counting unknown terms

--- recbole/model/general_recommender/jointsrmf.py
+++ recbole/model/general_recommender/jointsrmf.py
@@ -88,14 +88,12 @@ class JOINTSRMF(GeneralRecommender):
                     for term in desc.split():
                         if term in model.key_to_index:
                             wv_term_index = model.key_to_index[term]
-                        else:
-                            wv_term_index = model.key_to_index["unk"]
-                        if wv_term_index not in self.lm_gt_keys[item_id]:
-                            self.lm_gt_keys[item_id].append(wv_term_index)
-                            self.lm_gt_values[item_id].append(1)
-                        else:
-                            idx = self.lm_gt_keys[item_id].index(wv_term_index)
-                            self.lm_gt_values[item_id][idx] += 1
+                            if wv_term_index not in self.lm_gt_keys[item_id]:
+                                self.lm_gt_keys[item_id].append(wv_term_index)
+                                self.lm_gt_values[item_id].append(1)
+                            else:
+                                idx = self.lm_gt_keys[item_id].index(wv_term_index)
+                                self.lm_gt_values[item_id][idx] += 1
         self.logger.info(f"Done with lm_gt construction!")
 
         self.sigmoid = nn.Sigmoid()
